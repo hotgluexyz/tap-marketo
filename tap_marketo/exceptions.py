a391aa9c698c629f8b364d373857437f1bd35efe
Otavio Trentin <otavio_trentin@hotmail.com>
2026-03-10 11:29:47 -0300

    HGI-9879: add client.py last line; remove exceptions.py

--- tap_marketo/exceptions.py
+++
@@ -1,8 +0,0 @@
-from hotglue_singer_sdk.tap_base import InvalidCredentialsError
-
-__all__ = ["InvalidCredentialsError", "MissingPermissionsError"]
-
-
-class MissingPermissionsError(InvalidCredentialsError):
-    """Exception raised for missing permission."""
-    pass
